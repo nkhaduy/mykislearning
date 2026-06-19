4b4758fe5127c9e231c6c0fb7db6ff42254b6e07
Kha Duy <khaduy@bon-3.local>
2026-06-19 13:40:27 +0700

    Auto-sync: 2026-06-19 13:40:27

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ import {
   resetDemoHrAccount,
   resetPassword,
   unlockAccount,
+  updateAccount,
+  updateEmployeeProfile,
 } from "./lib/mockDatabase.js";
 import { validatePassword } from "./lib/auth/passwordPolicy.js";
 
