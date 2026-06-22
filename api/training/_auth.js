4ef405798e105e06ad9791a77ee744a5c952e5ed
Kha Duy <khaduy@realme-c11-2021.eps.com.vn>
2026-06-22 09:19:24 +0700

    Auto-sync: 2026-06-22 09:19:24

--- api/training/_auth.js
+++ api/training/_auth.js
@@ -36,6 +36,6 @@ export function requireAuth(req, res) {
 
 export function cors(res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Account-Id, X-Account-Role");
 }
